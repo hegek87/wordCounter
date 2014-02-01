a5753bcc001d9eb165442cfc8ffd5dbe9278de82
hegek87 <hegelandka@vcu.edu>
2014-02-01 03:34:35 -0500

    Mostly done with getting it to work for english characters. Need to add support for different character sets and improve the regex to properly split strings

--- WordCount.java
+++ WordCount.java
@@ -8,10 +8,13 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap; 
+import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class WordCount{
 	private BufferedReader reader;
-	private HashMap<String, Integer> wordCount;
+	private TreeMap<String, Integer> wordCount;
 	
 	/*
 	* Here we are assuming that a word is any string of 
@@ -20,10 +23,11 @@ public class WordCount{
 	* We can easily modify what is a word by simply
 	* changing this regex.
 	*/
-	private static final String REGEX = " *[^A-Za-z0-9_]+";
+	private static final int N_LARGEST_VALS = 11;
+	private static final String REGEX = "[^A-Za-z0-9_]+";
 	
 	public WordCount(){
-		wordCount = new HashMap<String, Integer>();
+		wordCount = new TreeMap<String, Integer>(String.CASE_INSENSITIVE_ORDER);
 	}
 	
 	// prepares the file to be read.
@@ -47,10 +51,9 @@ public class WordCount{
 	// process a single line of data
 	public void processLine(String singleLine){
 		String[] words = singleLine.split(REGEX);
-		System.out.println(words.length);
 		for(String s : words){
 			int numOfWords = 0;
-			Integer wordKey = wordCount.get(s.toLowerCase());
+			Integer wordKey = wordCount.get(s);
 			// word not found
 			if(wordKey == null){
 				numOfWords = 1;
@@ -60,7 +63,7 @@ public class WordCount{
 				numOfWords = wordKey+1;
 			}
 			// insert the new word, or update the count
-			wordCount.put(s.toLowerCase(), numOfWords);
+			wordCount.put(s, numOfWords);
 		}
 	}
 	
@@ -68,8 +71,42 @@ public class WordCount{
 		System.out.println("Usage: java [FILE NAME]");
 	}
 	
+	// This method is O(n), so it is faster than sorting
 	public void displayMostCommon(){
-		System.out.println(wordCount);
+		ArrayList<Map.Entry<String, Integer> > mostCommon;
+		mostCommon = new ArrayList<Map.Entry<String, Integer> >();
+		for(Map.Entry<String, Integer> el : wordCount.entrySet()){
+			// first add the first 10 elements sorted large to small
+			int i = 0;
+			if(mostCommon.isEmpty()){
+				mostCommon.add(el);
+			}
+			else if(i < N_LARGEST_VALS){
+				// insert the remaining elements in the correct order
+				int j = 0;
+				while(j < mostCommon.size() && el.getValue().intValue() 
+						< mostCommon.get(j).getValue().intValue()){
+					++j;
+				}
+				mostCommon.add(j, el);
+			}
+			else{
+				/*
+				* Now we can add elements to the front of the list
+				* when they are larger than the head, otherwise we
+				* can ignore them
+				*/
+				if(el.getValue().intValue() 
+						> mostCommon.get(0).getValue().intValue()){
+					mostCommon.add(el);
+				}
+			}
+		}
+		// Display the first ten elements
+		for(int i = 0; i < N_LARGEST_VALS; ++i){
+			if(i > mostCommon.size()){ break; }
+			System.out.println(mostCommon.get(i).getKey());
+		}
 	}
 	
 	public void processFile(){
@@ -83,6 +120,10 @@ public class WordCount{
 		}
 	}
 	
+	public void printMap(){
+		System.out.println(wordCount);
+	}
+	
 	public static void main(String[] args){
 		WordCount wc = new WordCount();
 		// No command line argument passed in
@@ -92,6 +133,7 @@ public class WordCount{
 		}
 		wc.openFile(args[0]);
 		wc.processFile();
+		//wc.printMap();
 		wc.displayMostCommon();
 		wc.closeFile();
 	}
