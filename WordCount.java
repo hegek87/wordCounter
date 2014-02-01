297b5a2bbbb3e64942f9d00f197a899519c32b67
hegek87 <hegelandka@vcu.edu>
2014-02-01 16:43:23 -0500

    Finished support for european languages. I believe it works for asian character sets but I can't verify it (not knowing any asian languages). The code read five lines of asian text and produces 10 output values (I am assuming that although these are long strings, they are words).

--- WordCount.java
+++ WordCount.java
@@ -11,8 +11,6 @@ import java.io.IOException;
 import java.util.TreeMap;
 import java.util.ArrayList;
 import java.util.Map;
-import java.text.Collator;
-import java.util.Locale;
 import java.nio.charset.StandardCharsets;
 
 public class WordCount{
@@ -23,32 +21,19 @@ public class WordCount{
 	* which takes a comparator as a constructor.
 	*/
 	private TreeMap<String, Integer> wordCount;
-	private Locale userLocale;
 	
 	/*
-	* Here we are assuming that a word is any string of 
-	* characters between any of the two punctuation
-	* characters in the below REGEX string.
-	* We can easily modify what is a word by simply
-	* changing this regex.
+	* We seperate our words by the below regex. If the character
+	* is none of the below (a number, apostrophe, underscore, or
+	* letter in any language) it is part of a word.
 	*/
-	private static final String REGEX = "[^A-Za-z0-9'_\\p{L}]+";
-	private static final int N_LARGEST_VALS = 10;
-	
-	/*
-	* We will use a collator to pass to the TreeMap to use for
-	* comparing strings of locale type userLocale
-	*/
-	public WordCount(Locale userLocale){
-		this.userLocale = userLocale;
-		Collator localeComp = Collator.getInstance(this.userLocale);
-		localeComp.setStrength(Collator.PRIMARY);
-		wordCount = 
-		new TreeMap<String, Integer>(localeComp);
-	}
+	private static final String REGEX = "[^0-9'_\\p{L}]+";
+	private static final int NUM_TO_DISPLAY = 10;
 	
 	public WordCount(){
-		this(Locale.getDefault());
+		wordCount = 
+			new TreeMap<String, Integer>
+				(String.CASE_INSENSITIVE_ORDER);
 	}
 	
 	// prepares the file to be read.
@@ -99,14 +84,13 @@ public class WordCount{
 	/*
 	* We will find the largest entry in our TreeMap and print
 	* the largest entries key, then remove it from the TreeMap.
-	* This will be done N_LARGEST_VAL times, and finding the 
-	* max is O(n), so this method is N_LARGEST_VAL*O(n)=O(n).
+	* This will be done NUM_TO_DISPLAY times, and finding the 
+	* max is O(n), so this method is NUM_TO_DISPLAY*O(n)=O(n).
 	*/
 	public void displayMostCommon(){
-	System.out.println(wordCount.size());
-		for(int i = 0; i < N_LARGEST_VALS && !wordCount.isEmpty(); ++i){
+		for(int i = 0; i < NUM_TO_DISPLAY && !wordCount.isEmpty(); ++i){
 			Map.Entry<String, Integer> curMax = keyWithMaxValue();
-			System.out.println(curMax.getKey()+"\t"+curMax.getValue());
+			System.out.println(curMax.getKey());
 			wordCount.remove(curMax.getKey());
 		}
 	}
@@ -140,10 +124,9 @@ public class WordCount{
 			while((curLine = reader.readLine()) != null){
 				// we want to ignore any lines of only white space
 				if(!curLine.trim().isEmpty()){
-					String utfLine = new String(curLine.getBytes(), "UTF-8");
+					String utfLine = 
+						new String(curLine.getBytes(), "UTF-8");
 					processLine(utfLine.trim());
-					int i = 1;
-					System.out.println(i++ + utfLine);
 				}
 			}
 		} catch(IOException ioe){
@@ -152,7 +135,7 @@ public class WordCount{
 	}
 	
 	public static void main(String[] args){
-		WordCount wc = new WordCount(Locale.JAPANESE);
+		WordCount wc = new WordCount();
 		// No command line argument passed in
 		if(args.length != 1){
 			usage();
